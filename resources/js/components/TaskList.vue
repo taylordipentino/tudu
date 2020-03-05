cd3f6cb2bdf2cbfa7e70a2b5637543bec32d6f94
Taylor DiPentino <taylor.dipentino@gmail.com>
2020-03-04 19:32:36 -0500

    Moved loading of tasks to own method in Task List component.

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -37,6 +37,41 @@
             }
         }, 
         methods: {
+            load() {
+                // Clear any previous errors 
+                this.error = false;
+
+                // Clear any previous tasks
+                this.tasks = [];
+
+                // Create the request
+                fetch('task/read')
+                    .then((response) => {
+                        // Make sure there isn't a non-200 status
+                        if (response.status !== 200) {
+                            // Provide an error message
+                            this.error = 'The server encountered an error while processing your request.';
+                            // Don't continue
+                            return false;
+                        }
+
+                        // Otherwise return the JSON response
+                        return response.json();
+                    })
+                    .then((data) => {
+                        // Check if tasks were returned
+                        if (data.length > 0) {
+                            // Let the component know there are tasks
+                            this.hasTasks = true;
+                            // Store the tasks
+                            this.tasks = data;
+                            return;
+                        }
+
+                        // Otherwise let the component know there are no tasks
+                        this.hasTasks = false;
+                    });
+            }, 
             remove(id) {
                 // Clear any previous errors 
                 this.error = false;
@@ -60,24 +95,13 @@
                         }
 
                         // Reload the Task List
-                        this.$router.go();
+                        this.load();
                     });
             }
         }, 
         mounted() {
-            fetch('task/read')
-                .then((response) => {
-                    return response.json();
-                })
-                .then((data) => {
-                    if (data.length > 0) {
-                        this.hasTasks = true;
-                        this.tasks = data;
-                        return;
-                    }
-
-                    this.hasTasks = false;
-                });
+            // Load the Task List data
+            this.load();
         }
     }
 </script>
